6f07c1cc89b83418004eb62db56a740b2059f8a8
U-AMDX34GB-PC\papa <muh_kosta@mail.ru>
2019-11-22 23:45:22 +0300

    java conv

--- Matrix_err.py
+++ Matrix_err.py
@@ -1,16 +1,34 @@
 matrix=[[1,2,4,5],
         [6,7,8,9]] # матрица (2,4)
 
-errs=[10,20]
-    # матрица (2,1)
+import  numpy as np
+np.random.seed(42)
+matr=np.random.randint(0,100,(4,4))
+kern=np.random.randint(0,100,(2,2))
+print("kern:\n",kern)
+print("matr:\n",matr)
+kern=kern.tolist()
+matr_as_ribbon=matr.flatten().tolist()
+def conv(matr_as_ribbon_one_channel, kernel_as_matr_one_channel):
+    matr_as_ribbon_height=4
+    matr_as_ribbon_width=4
+    kernel_as_matr_one_channel_width=2
+    S=1 # шаг
+    V=((matr_as_ribbon_width - kernel_as_matr_one_channel_width) // S + 1) # выходной обьем(ширина) выходной карты признаков
+    # выходная карта признаков квадратная
+    result_conv_one_channel_as_ribbon=[0]*(V**2)
+    n=-1
+    for i in range(matr_as_ribbon_height-1):
+        for j in range(matr_as_ribbon_width-1):
+            result=0
+            n += S
+            for a in range(kernel_as_matr_one_channel_width):
+                for b in range(kernel_as_matr_one_channel_width):
+                    Y=i*S+a
+                    X=j*S+b
+                    print(matr_as_ribbon_one_channel[matr_as_ribbon_width*Y+X])
+                    result+=matr_as_ribbon_one_channel[matr_as_ribbon_width*Y+X]*kernel_as_matr_one_channel[a][b]
+                    result_conv_one_channel_as_ribbon[n]=result
+    return result_conv_one_channel_as_ribbon
 
-matr_res=[0]*len(matrix[0])
-
-for elem in range(len(matrix[0])):
-     #   matr_res[elem]=0
-        for row in range(len(matrix)):
-                matr_res[elem]+=errs[row] * matrix[row][elem]
-
-
-
-print (matr_res)
+print(conv(matr_as_ribbon,kern))
--- java/JavaApplication1/src/javaapplication1/JavaApplication1.java
+++ java/JavaApplication1/src/javaapplication1/JavaApplication1.java
@@ -1,53 +1,114 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package javaapplication1;
 import java.awt.Image;
 import java.awt.image.BufferedImage;
-import java.awt.image.ConvolveOp;
-import java.awt.image.Kernel;
 import java.io.ByteArrayOutputStream;
 import java.io.File;
 import java.io.IOException;
 import javax.imageio.ImageIO;
-/**
- *
- * @author papa
- */
 public class JavaApplication1 {
-    float[] IDENTITY = {0f, 0f, 0f, 0f, 1f, 0f, 0f, 0f, 0f};
-    BufferedImage normal;
-    BufferedImage current;
-    Image image;
-    /**
-     * @param args the command line arguments
-     */
-    public BufferedImage apply(String fName) {
-        // TODO code application logic here
+    int w=0;// ширина входного изображения
+    int h=0;// его высота
+    float[] imageInputR=new float [w*h];
+    float[] imageInputG=new float [w*h];
+    float[] imageInputB=new float [w*h];
+    /*
+    Произвольные веса
+    */
+    float[][] kernel = new float[][]{{1.0f, 0.8f, 0.6f}, {0.1f, 0.5f, 0.4f}, {0.3f, 0.5f, 0.7f}};
+    /*
+    Сворачивать по отдельности в отдельную карту признаков,
+    т.к. сложно представить сворачивание с 3D ядром.
+    */
+    float[] featuredMapR;
+    float[] featuredMapG;
+    float[] featuredMapB;
+    public  void work() {
+        byte[] imageInputRByte = getByteArray(open("test.png", "R"), "png");
+        for (int i = 0; i < imageInputRByte.length; i++) {
+            byte b = imageInputRByte[i];
+            imageInputR[i]=(float)(b/255.0f);
+        }
+        featuredMapR=conv(imageInputR, w, h, kernel,1, -1);
+    }
+    public BufferedImage open(String fName, String whatChannel) {
+        Image image = null;
         try {
             File pathToFile = new File(fName);
             image = ImageIO.read(pathToFile);
         } catch (IOException ex) {
             ex.printStackTrace();
         }
-        int w = image.getWidth(null);
-        int h = image.getHeight(null);
-        normal = new BufferedImage(w, h, BufferedImage.TYPE_INT_RGB);
-        Kernel kernel = new Kernel(3, 3, IDENTITY);
-        ConvolveOp co = new ConvolveOp(kernel);
-        current = co.filter(normal, null);
-        return current;
+        w = image.getWidth(null);
+        h = image.getHeight(null);
+        BufferedImage C = new BufferedImage(
+                w, h,
+                BufferedImage.TYPE_INT_RGB
+        );
+        int rgb = 0;
+        for (int x = 0; x < w; x++) {
+            for (int y = 0; y < h; y++) {
+                rgb = C.getRGB(x, y);
+                switch (whatChannel) {
+                    case "R":
+                        C.setRGB(x, y, rgb & 0xff0000);
+                    case "G":
+                        C.setRGB(x, y, rgb & 0xff00);
+                    case "B":
+                        C.setRGB(x, y, rgb & 0xff);
+                    default:
+                        throw new AssertionError();
+                }
+            }
+        }
+        return C;
     }
-    public byte[] getByteArray(BufferedImage image) {
-        ByteArrayOutputStream out=null;
+    public byte[] getByteArray(BufferedImage image, String ext) {
+        ByteArrayOutputStream out = null;
         try {
             out = new ByteArrayOutputStream();
-            ImageIO.write(image, "png", out);
+            ImageIO.write(image, ext, out);
         } catch (IOException ex) {
             ex.printStackTrace();
         }
         return out.toByteArray();
+    }
+    /**
+     * Сворачивает 'один' канал(т.к. kernel 2d)
+     *
+     * @param input изображение как лента
+     * @param width ширина изначального изображения
+     * @param height его высота
+     * @param kernel 2d ядро
+     * @param stride шаг при свертке
+     * @param isConv 1 для конвульции и -1 для кросс-кореляции
+     * @return выходная карта признаков как лента
+     */
+    public float[] conv(float[] input, int width, int height, float[][] kernel, int stride, int isConv) {
+        int kernelWidth = kernel[0].length;
+        int kernelHeight = kernel.length;
+        int V = (width - kernelWidth / stride) + 1;// выходной обьем(ширина) выходной карты признаков
+        float output[] = new float[V * V]; // выходная карта признаков квадратная
+        int n = -1;
+        //Производим вычисления
+        for (int i = 0; i < height - 1; i++) {
+            for (int j = 0; j < width - 1; j++) {
+                float result = 0;
+                n += stride;
+                for (int a = 0; a < kernelHeight; a++) {
+                    for (int b = 0; b < kernelWidth; b++) {
+                        int Y = i * stride + a;
+                        int X = j * stride + b;
+                        // для прямого распространения - кросс-кореляция,для обратного-ковулция
+                        result += input[width * Y - isConv * X] * kernel[a][b];
+                        output[n] = result;
+                    }
+                }
+            }
+        }
+        return output;
+    }
+    public static void main(String[] args) {
+        JavaApplication1 app=new JavaApplication1();
+        app.work();
     }
 }
